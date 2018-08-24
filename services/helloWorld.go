753afe3627e67b2255753ef07902df302ab0e841
michael.golowka <michael.golowka@logrhythm.com>
2018-08-23 18:17:14 -0600

    Add new feature

--- services/helloWorld.go
+++ services/helloWorld.go
@@ -7,5 +7,5 @@ import (
 
 func HelloWorld(rw http.ResponseWriter, req *http.Request) {
 	rw.WriteHeader(http.StatusOK)
-	fmt.Fprintln(rw, "Hello, LogRhythm Hackathon!")
+	fmt.Fprintln(rw, "Hello, LogRhythm Hackathon! This is a new feature!")
 }
